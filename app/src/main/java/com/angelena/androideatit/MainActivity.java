37b71d23182bc43805fe5ac7c1be385ea4ffccfe
Angelena Ward <alw1079@gmail.com>
2018-10-02 22:26:19 -0400

    android eat it app to order food

---
+++ app/src/main/java/com/angelena/androideatit/MainActivity.java
@@ -0,0 +1,24 @@
+package com.angelena.androideatit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
+    TextView txtSlogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan)
+
+
+    }
+}
