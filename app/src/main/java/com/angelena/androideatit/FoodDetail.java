9935e838520dd396895043ffaffe75d277fc9f49
Angelena Ward <alw1079@gmail.com>
2018-10-28 00:10:04 -0400

    show food details

---
+++ app/src/main/java/com/angelena/androideatit/FoodDetail.java
@@ -0,0 +1,45 @@
+package com.angelena.androideatit;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FoodDetail extends AppCompatActivity {
+
+    TextView food_name, food_price, food_description;
+    ImageView food_image;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    FloatingActionButton btnCart;
+    ElegantNumberButton numberButton;
+
+    String foodId = " ";
+
+    FirebaseDatabase database;
+    DatabaseReference foods;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_detail);
+
+        //firebase
+        database = FirebaseDatabase.getInstance();
+        foods = database.getReference("Foods");
+
+        numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
+        btnCart = (FloatingActionButton)findViewById(R.id.btnCart);
+
+        food_description = (TextView)findViewById(R.id.food_description);
+        food_name = (TextView)findViewById(R.id.food_name);
+        food_price = (TextView)findViewById(R.id.food_price);
+        food_image = (ImageView)findViewById(R.id.food_image);
+
+    }
+}
