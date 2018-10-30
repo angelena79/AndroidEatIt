a34a113cb4ea7e57a7f4184d3cc9c54925ffab31
Angelena Ward <alw1079@gmail.com>
2018-10-30 03:49:51 -0400

    Food description details added

--- app/src/main/java/com/angelena/androideatit/FoodDetail.java
+++ app/src/main/java/com/angelena/androideatit/FoodDetail.java
@@ -1,5 +1,6 @@
 package com.angelena.androideatit;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,14 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.angelena.androideatit.Model.Food;
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class FoodDetail extends AppCompatActivity {
 
@@ -33,13 +39,53 @@ public class FoodDetail extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         foods = database.getReference("Foods");
 
+        //init view
         numberButton = (ElegantNumberButton)findViewById(R.id.number_button);
         btnCart = (FloatingActionButton)findViewById(R.id.btnCart);
 
         food_description = (TextView)findViewById(R.id.food_description);
         food_name = (TextView)findViewById(R.id.food_name);
         food_price = (TextView)findViewById(R.id.food_price);
-        food_image = (ImageView)findViewById(R.id.food_image);
+        food_image = (ImageView)findViewById(R.id.img_food);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+
+        //GEt food id from intent
+        if (getIntent() != null)
+            foodId = getIntent().getStringExtra("FoodId");
+        if (!foodId.isEmpty()){
+            getDetialFood(foodId);
+        }
 
     }
+
+    private void getDetialFood(String foodId) {
+        foods.child(foodId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Food food = dataSnapshot.getValue(Food.class);
+
+                //Set Image
+                Picasso.with(getBaseContext()).load(food.getImage())
+                        .into(food_image);
+
+                collapsingToolbarLayout.setTitle(food.getName());
+
+                food_price.setText(food.getPrice());
+
+                food_name.setText(food.getName());
+
+                food_description.setText(food.getDescription());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
